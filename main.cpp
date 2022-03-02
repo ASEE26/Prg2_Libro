1bfbd81250e902ffcab02e5f07225fc5dd79bed3
= <pabloesau26@gmail.com>
2022-03-01 19:37:25 -0600

    Funciones ingresar y buscar
    Se ordenó el código para ingresar y ordenar libros por funciones

--- main.cpp
+++ main.cpp
@@ -5,22 +5,19 @@ using std::cin;
 
 #include "Libro.hpp"
 
+void ingresarLibros(Libro *, int); // Prototipo de la función
+void bucarLibro(Libro *, int);
+
 
 int main(){
 
 int opcion;
-int continuar = 1;
 int cantidadLibros;
-bool estaLibro = false;
-string titulo, tema;
 string autor;
 
 // Memoria dinamica.
 Libro *libros;
 //Libro libros[2];
-
-int contador= 0;
-
 
 do{
 cout<<"Sistema bibleotecario"<<endl;
@@ -36,44 +33,13 @@ case  1:
 
         cout<<"Cuantos Libros desea registrar: ";
         cin >> cantidadLibros;
-        libros = new Libro[cantidadLibros];
-
-        for (int  i = 0; i < cantidadLibros; i++)
-        {
-            cout<<"Ingrese el titulo del Libro "<<i+1<<"  : ";
-            cin >> titulo;
-            cout<<"Ingrese el tema: ";
-            cin >> tema;
-            //cout<<"Ingrese al autor: "<<endl;
-            //cin >> autor;
-
-            libros[i].establecerTema(tema);
-            libros[i].establecerTitulo(titulo);
-            cout<<"Libro ingresado con exito"<<endl;
-            cout<<"*******************************"<<endl;
-
-        }
+        libros = new Libro[cantidadLibros]; // Arreglo de libros, pero vacio. Sin libros.
+        ingresarLibros(libros,cantidadLibros);
+      cout<<"Cantiodad de libros despues de ingresar libros: "<<cantidadLibros<<endl;
     break;
 
   case 2:
-            cout<<"Ingrese el tema del libro a buscar: ";
-            cin  >> tema;
-            estaLibro = false;
-            for (int  i = 0; i < cantidadLibros; i++)
-            {   
-                    if(libros[i].obtenerTema() == tema){
-                        cout<<"***************************************"<<endl;
-                        cout<<"El libro que busca se encuentrra:"<<endl;
-                        cout<<"Tema: "<<libros[i].obtenerTema()<<endl;
-                        cout<<"Titulo: "<<libros[i].obtenerTitulo()<<endl;
-                        //cout<<"Autores: "<<L2.onte
-                        estaLibro = true;
-                    }
-              }
-              if(!estaLibro){
-                  cout<<"EL LIBRO NO SE ENCUENTRA REGISTRADO"<<endl;
-              }
-
+           bucarLibro(libros,cantidadLibros);
 
              break;
 
@@ -84,4 +50,52 @@ cout<<"________________________________________________"<<endl<<endl;
 }while(true);
 
     return 0;
+}
+
+
+void ingresarLibros(Libro *L, int n){
+     string titulo, tema;
+
+  for (int  i = 0; i < n; i++)
+        {
+            cout<<"Ingrese el titulo del Libro "<<i+1<<"  : ";
+            cin >> titulo;
+            cout<<"Ingrese el tema: ";
+            cin >> tema;
+            //cout<<"Ingrese al autor: "<<endl;
+            //cin >> autor;
+
+            L[i].establecerTema(tema);
+            L[i].establecerTitulo(titulo);
+            cout<<"Libro ingresado con exito"<<endl;
+            cout<<"*******************************"<<endl;
+
+        }
+        n=10000;
+
+}
+
+void bucarLibro(Libro *L, int n){
+        string titulo, tema;
+        bool estaLibro = false;
+
+        cout<<"Ingrese el tema del libro a buscar: ";
+            cin  >> tema;
+            estaLibro = false;
+            for (int  i = 0; i < n; i++)
+            {   
+                    if(L[i].obtenerTema() == tema){
+                        cout<<"***************************************"<<endl;
+                        cout<<"El libro que busca se encuentrra:"<<endl;
+                        cout<<"Tema: "<<L[i].obtenerTema()<<endl;
+                        cout<<"Titulo: "<<L[i].obtenerTitulo()<<endl;
+                        //cout<<"Autores: "<<L2.onte
+                        estaLibro = true;
+                    }
+              }
+              if(!estaLibro){
+                  cout<<"EL LIBRO NO SE ENCUENTRA REGISTRADO"<<endl;
+              }
+
+
 }
